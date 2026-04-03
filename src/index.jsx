5b96bd6884e067894c655b45ad74da011affdd13
aspandiyars <139233664+aspandiyars@users.noreply.github.com>
2026-04-04 00:06:19 +0500

    adding index jsx

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Footer from './components/Footer';
+
+const footerRoot = document.getElementById('footer-root');
+if (footerRoot) {
+    ReactDOM.render(<Footer />, footerRoot);
+}

4d71fa719a08233c223636a790a77eb5fdbf0c82
aspandiyars <139233664+aspandiyars@users.noreply.github.com>
2026-04-04 00:31:52 +0500

    Delete src directory

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Footer from './components/Footer.jsx';
-
-const footerRoot = document.getElementById('footer-root');
-if (footerRoot) {
-    ReactDOM.render(<Footer />, footerRoot);
-}
